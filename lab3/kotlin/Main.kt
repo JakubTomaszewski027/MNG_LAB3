a439cfffaf4209b93b0f77b8ab9267f966cd868f
JakubTomaszewski027 <115466427+JakubTomaszewski027@users.noreply.github.com>
2023-05-09 07:08:23 -0400

    Add files via upload

---
+++ lab3/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val arr = intArrayOf(3, 7, 19, 2, 6, 27)
+    bubbleSort(arr)
+}
+
+fun bubbleSort(arr: IntArray) {
+    val n = arr.size
+    for (i in 0 until n - 1) {
+        for (j in 0 until n - i - 1) {
+            if (arr[j] > arr[j + 1]) {
+                val temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+}
